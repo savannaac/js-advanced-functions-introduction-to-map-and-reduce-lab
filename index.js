2252c21c8a2ecc7812591fb4760c32c8189df87d
savanna chiang <savannaac@gmail.com>
2021-07-20 23:21:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+// map methods
+function mapToNegativize(array) {
+    return array.map(i => i * -1)
+}
+
+function mapToNoChange(array) {
+    return array.map(i => i)
+}
+
+function mapToDouble(array) {
+    return array.map(i => i * 2)
+}
+
+function mapToSquare(array) {
+    return array.map(i => i * i)
+}
+
+// reduce methods
+function reduceToTotal(array, index = 0) {
+    return array.reduce((x, y) => x + y, index)
+}
+
+function reduceToAllTrue(array) {
+    return array.reduce((value) => Boolean(value))
+}
+
+function reduceToAnyTrue(array) {
+    return array.reduce((value) => Boolean(value), true)
+}
+
+
